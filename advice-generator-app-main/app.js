d6fa9a543fba45721037f38061442c2de641ff38
Aryan Khatik <rkhatik156@gmail.com>
2023-03-27 14:01:07 +0530

    fixed bugs

--- advice-generator-app-main/app.js
+++ advice-generator-app-main/app.js
@@ -1,22 +1,26 @@
-let fetchdata = new XMLHttpRequest();
 
-fetchdata.open('GET','https://api.adviceslip.com/advice',true);
-
-
-fetchdata.onload=()=>{
-    if (fetchdata.status===200) {
-        let data = JSON.parse(fetchdata.responseText);
-        document.getElementById('Advice').innerHTML += data.slip.id;
-       document.getElementById('Note').innerHTML=`"${data.slip.advice}"`;
-    }
-    
-    
-}
-fetchdata.send();
 
 let button=document.getElementById('but');
 button.addEventListener('click',Reload);
+let note = document.getElementById('Note');
 
 function Reload(){
-    location.reload();
+    let fetchdata = new XMLHttpRequest();
+
+    fetchdata.open('GET','https://api.adviceslip.com/advice',true);
+    
+    
+    fetchdata.onload=()=>{
+        if (fetchdata.status===200) {
+            let data = JSON.parse(fetchdata.responseText);
+          
+            document.getElementById('Advice').innerHTML = data.slip.id;
+           document.getElementById('Note').innerHTML=`"${data.slip.advice}"`;
+           note.classList.add('note2');
+
+        }
+
+    }
+    fetchdata.send();
 }
+Reload();
